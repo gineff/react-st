5da6c2306679badefcca511eb5d4ae968df73734
gineff <canone@inbox.ru>
2025-01-02 19:37:22 +0300

    ft: add action

---
+++ src/pages/actions/actionsPage.tsx
@@ -0,0 +1,14 @@
+export const ActionsPage = () => {
+
+  //useOptimistic
+  //useTransitions
+  //useDeferredValue
+  //useActionsState
+  //useFormStatus
+  
+  return (
+    <>
+      Actions page
+    </>
+  );
+};
