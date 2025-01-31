509d413d9e1bdfadebfcb1816e1a131a478ad918
gineff <canone@inbox.ru>
2025-01-31 10:18:37 +0300

    ft: update param editor

--- src/pages/param-editor/paramEditor.tsx
+++ src/pages/param-editor/paramEditor.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// Интерфейсы структур данных
 interface Param {
   id: number;
   name: string;
@@ -13,7 +12,6 @@ interface ParamValue {
 }
 
 interface Color {
-  // Предположим, что это просто строка для цвета
   name: string;
 }
 
@@ -67,7 +65,7 @@ class ParamEditor extends React.Component<Props, State> {
 
     return {
       paramValues: updatedParamValues,
-      colors: this.props.model.colors, // Не меняем colors, так как они не редактируются
+      colors: this.props.model.colors,
     };
   }
 
@@ -76,17 +74,38 @@ class ParamEditor extends React.Component<Props, State> {
     const { paramValues } = this.state;
 
     return (
-      <div>
-        {params.map((param) => (
-          <div key={param.id}>
-            <label>{param.name}</label>
-            <input
-              type="text"
-              value={paramValues[param.id] || ''}
-              onChange={(e) => this.handleInputChange(param.id, e)}
-            />
-          </div>
-        ))}
+      <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-6">Редактор параметров</h2>
+        <div className="space-y-4">
+          {params.map((param) => (
+            <div key={param.id} className="flex flex-col">
+              <label
+                htmlFor={`param-${param.id}`}
+                className="text-sm font-medium text-gray-700 mb-1"
+              >
+                {param.name}
+              </label>
+              <input
+                id={`param-${param.id}`}
+                type="text"
+                value={paramValues[param.id] || ''}
+                onChange={(e) => this.handleInputChange(param.id, e)}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm 
+                focus:border-blue-500 focus:ring-blue-500 
+                p-2 border"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="mt-6">
+          <button
+            onClick={() => console.log(this.getModel())}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md 
+                     hover:bg-blue-600 transition-colors"
+          >
+            Получить модель
+          </button>
+        </div>
       </div>
     );
   }
